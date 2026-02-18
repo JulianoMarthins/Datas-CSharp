c0779d5bfe64be95162ff0f984d5b0355dfb2b85
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2026-02-18 11:00:46 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿Console.Clear();
+﻿using System.Data;
+using System.Globalization;
+using Microsoft.VisualBasic;
+
+Console.Clear();
 
 Console.WriteLine("Desenvolvido por Juliano Martins\n\n");
 
@@ -17,4 +21,66 @@ DateTime data = new DateTime(1982, 6, 4);
 string dataFormatada = String.Format("{0:D}", data);
 Console.WriteLine($"Meu aniversário: {dataFormatada}");
 
+// Podemos usar códigos para descrever o estilo de formatação pré definidos
+
+// Somente a hora e minutos
+Console.WriteLine(String.Format("{0:t}", nascimento));
+
+// Horario completo com hora, minutos e segundos
+Console.WriteLine(String.Format("{0:T}", nascimento));
+
+// Data 
+Console.WriteLine(String.Format("{0:d}", nascimento));
+
+// Data completa com dia por extenso, número do dia mês por extenso e ano
+Console.WriteLine(String.Format("{0:D}", nascimento));
+
+// Data no padrão para uso do Json
+Console.WriteLine(String.Format("{0:s}", nascimento));
+
+// Padrão de data que muitos sistemas usam
+Console.WriteLine(String.Format("{0:r}", nascimento));
+
+
 Console.ReadKey();
+Console.Clear();
+
+Console.WriteLine("Soma de dias as datas: ");
+
+var date = DateTime.Now;
+Console.WriteLine($"Dia de hoje: {date}");
+
+Console.WriteLine($"Acrescentado 12 dias: {date.AddDays(-12)}");
+
+Console.WriteLine($"Acrescentado 2 messes: {date.AddMonths(2)}");
+
+Console.WriteLine($"Acrescentado 4 anos: {date.AddYears(4)}");
+
+Console.WriteLine($"Acrescentado 8 horas: {date.AddHours(8)}");
+
+
+Console.ReadKey();
+Console.Clear();
+
+Console.WriteLine("Comparando datas");
+
+
+DateTime data2 = DateTime.Now;
+
+if (data2.Date == DateTime.Now.Date)
+{
+    Console.WriteLine("As datas são iguais");
+}
+
+
+Console.ReadKey();
+Console.Clear();
+
+
+Console.WriteLine("CultureInfo");
+
+var pt = new System.Globalization.CultureInfo("pt=BR");
+var en = new System.Globalization.CultureInfo("en-US");
+
+Console.WriteLine(DateTime.Now.ToString("D"));
+
